7d28f39efdcbfb4eb6e31b053f4c996360d967ff
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-04 13:10:34 -0700

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Password Encryption and Validation Module
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+        Generates a salted and hashed password.
+
+        Args:
+                password (str): A string containing the plain text
+                password to be hashed.
+
+        Returns:
+                bytes: A byte string representing the salted, hashed password.
+        """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+
+    return hashed

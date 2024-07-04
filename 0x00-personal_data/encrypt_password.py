733ba29c661b1bb37c527eaba4b0e59ba85ae27e
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-04 13:23:34 -0700

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -20,3 +20,14 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Validates whether the provided password matches the hashed password.
+    """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid

1449dd0a0199753be60cc6fbf9df220dcea0d2ec
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-11 14:16:58 -0700

    Basic Authentication

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,10 +27,11 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    user = User.get(user_id)
-    if user is None:
+    if user_id == "me" and request.current_user is None:
         abort(404)
-    return jsonify(user.to_json())
+    if user_id == "me" and request.current_user is not None:
+        user = request.current_user
+        return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)

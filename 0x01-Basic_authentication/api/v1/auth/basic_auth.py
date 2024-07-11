f21fd7eb4eafe0ffed69e06030a525a5a885954e
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-11 12:55:38 -0700

    Basic - Allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,9 +66,9 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        user_credentials = decoded_base64_authorization_header.split(":", 1)
+        user_credentials = decoded_base64_authorization_header.split(':', 1)
 
-        return user_credentials[0], user_credenetials[1]
+        return user_credentials[0], user_credentials[1]
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):

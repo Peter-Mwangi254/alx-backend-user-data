486ee084b89afb2744bba86a99ed2b2c7a772a20
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-12 10:22:14 -0700

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+class SessionAuth that inherits form Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    class SessionAuth that inherits from Auth
+    """
+    pass

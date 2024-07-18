e2625176122af95a9fa5e9f021ddf2ba9e13fae1
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-18 02:20:15 -0700

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Authentication system
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
